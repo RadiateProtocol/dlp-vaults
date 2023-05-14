7472eb91e34328575d9607c25c1da10e4a1e905c
xiao <radiateprotocol@protonmail.com>
2023-05-13 19:55:22 -0700

    add more kernel logic

---
+++ src/modules/TOKEN/RADToken.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.12;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "src/kernel.sol";
+
+/// @title RADToken
+/// @notice RADToken is a contract for the RAD token.
+contract RADToken is ERC20, Module {
+    //============================================================================================//
+    //                                        MODULE SETUP                                        //
+    //============================================================================================//
+
+    /// @notice Initializes the contract.
+    constructor(
+        Kernel kernel_
+    ) ERC20("Radiance Token", "RAD") Module(kernel_) {}
+
+    /// @inheritdoc Module
+    function KEYCODE() public pure override returns (Keycode) {
+        return toKeycode("TOKEN");
+    }
+
+    /// @inheritdoc Module
+    function VERSION()
+        external
+        pure
+        override
+        returns (uint8 major, uint8 minor)
+    {
+        major = 1;
+        minor = 0;
+    }
+
+    //============================================================================================//
+    //                                       CORE FUNCTIONS                                       //
+    //============================================================================================//
+
+    function mint(address _to, uint256 _amount) external permissioned {
+        _mint(_to, _amount);
+    }
+}
