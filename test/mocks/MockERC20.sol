3b0454d5c7858d1a3f5fdde91cdd8a332d3a7a97
w <w@gmi.com>
2023-05-27 19:08:32 -0700

    stakechef contract done, but LVGVAULT started

--- test/mocks/MockERC20.sol
+++ test/mocks/MockERC20.sol
@@ -1,12 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@solmate/tokens/ERC20.sol";
 
 contract MockERC20 is ERC20 {
-    constructor(string memory name, string memory symbol, uint256 initialSupply) ERC20(name, symbol) {
-        _mint(msg.sender, initialSupply);
-    }
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint8 decimals_
+    ) ERC20(name, symbol, decimals_) {}
+    
 
     function mint(address to, uint256 amount) external {
         _mint(to, amount);
