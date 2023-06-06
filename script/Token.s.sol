334cd60b7dda9ac1762d6ebff95b2d4833a23971
w <w@gmi.com>
2023-06-06 12:55:22 -0700

    add abi

--- script/Token.s.sol
+++ script/Token.s.sol
@@ -2,44 +2,49 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
-import "src/policies/RolesAdmin.sol";
 import "src/Kernel.sol";
-import {RADPresale} from "src/launch_contracts/PresaleContractFlattened.sol";
-import "src/modules/ROLES/OlympusRoles.sol";
-import "src/modules/TOKEN/RADToken.sol";
-import "src/modules/TRSRY/TRSRY.sol";
-import "src/policies/Initialization.sol";
-import "src/Kernel.sol";
+import {RolesAdmin} from "src/policies/RolesAdmin.sol";
+import "src/launch_contracts/PresaleContractFlattened.sol";
+import {OlympusRoles} from "src/modules/ROLES/OlympusRoles.sol";
+import {RADToken as Token} from "src/modules/TOKEN/RADToken.sol";
+import {Treasury} from "src/modules/TRSRY/TRSRY.sol";
+import {Initialization} from "src/policies/Initialization.sol";
 
-import {ERC20} from "@solmate/tokens/ERC20.sol";
-import {Console2} from "forge-std/Console2.sol";
-
-contract CounterScript is Script {
+contract TokenScript is Script {
     function setUp() public {}
 
     function run() public {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
-        address multisig = vm.envUint("RAD_MULTISIG");
+        address multisig = vm.envAddress("RAD_MULTISIG");
         vm.startBroadcast(deployerPrivateKey);
         Kernel kernel = new Kernel();
         Token token = new Token(kernel);
+        console2.log("Kernel address: ", address(kernel));
+
         Treasury treasury = new Treasury(kernel);
         OlympusRoles roles = new OlympusRoles(kernel);
         kernel.executeAction(Actions.InstallModule, address(roles));
+        console2.log("Roles address: ", address(roles));
+
         kernel.executeAction(Actions.InstallModule, address(treasury));
+        console2.log("Treasury address: ", address(roles));
         kernel.executeAction(Actions.InstallModule, address(token));
+        console2.log("Token address: ", address(roles));
         // Activate Policies
         RolesAdmin rolesAdmin = new RolesAdmin(kernel);
+        console2.log("Roles Admin address: ", address(roles));
         Initialization initialization = new Initialization(
             kernel,
             vm.addr(deployerPrivateKey)
         );
+        console2.log("Initialization address: ", address(roles));
 
         kernel.executeAction(Actions.ActivatePolicy, address(rolesAdmin));
         kernel.executeAction(Actions.ActivatePolicy, address(initialization));
 
         // deploy presale contracts
-        RADPresale presale = new RADPresale(address(token), multisig);
+        RADPresale presale = new RADPresale(multisig, IERC20(address(token)));
+        console2.log("Presale address: ", address(presale));
 
         kernel.executeAction(Actions.ChangeExecutor, multisig);
         // Set up vesting (later)
--- src/Migrator.sol
+++ src/Migrator.sol
@@ -30,20 +30,20 @@ contract MigratorZap is IFlashLoanSimpleReceiver, Ownable {
     constructor() {}
 
     function addLeverager(address _asset, address _leverager) public onlyOwner {
-        leveragers[_asset] = Leverager(_leverager);
+        leveragers[_asset] = Leverager(payable(_leverager));
     }
 
     // No ETH unlooping support
     function migrate(uint256 _amount, address _asset) public {
         bytes memory params = "";
-        bool success = aaveLendingPool.flashLoanSimple(
+        aaveLendingPool.flashLoanSimple(
             address(this),
             _asset,
             _amount,
-            params
+            params,
+            0
         );
-        require(success, "Migrator: Flashloan failed");
-        Leverager leverager = leveragers[asset];
+        Leverager leverager = leveragers[_asset];
         leverager.deposit(ERC20(_asset).balanceOf(address(this)), msg.sender);
         emit Migrate(msg.sender, _amount, _asset);
     }
@@ -60,17 +60,24 @@ contract MigratorZap is IFlashLoanSimpleReceiver, Ownable {
         }
         if (initiator != address(this))
             revert Migrator_ONLY_SELF_INIT(initiator);
-        if (asset.allowance(address(this), address(aaveLendingPool)) == 0) {
+        if (
+            ERC20(asset).allowance(address(this), address(aaveLendingPool)) == 0
+        ) {
             ERC20(asset).approve(address(aaveLendingPool), type(uint256).max);
         }
-        if (asset.allowance(address(this), address(radiantLendingPool)) == 0) {
+        if (
+            ERC20(asset).allowance(
+                address(this),
+                address(radiantLendingPool)
+            ) == 0
+        ) {
             ERC20(asset).approve(
                 address(radiantLendingPool),
                 type(uint256).max
             );
         }
 
-        lendingPool.repay(asset, amount, 2, msg.sender);
-        lendingPool.withdraw(asset, amount, msg.sender);
+        radiantLendingPool.repay(asset, amount, 2, msg.sender);
+        radiantLendingPool.withdraw(asset, amount, msg.sender);
     }
 }
