a43d4ffea251a37a533b9d709f47a6d4d432fb9c
r <radaiateprotocol@proton.me>
2023-06-05 17:11:28 -0700

    add migration

--- script/Counter.s.sol
+++ script/Counter.s.sol
@@ -2,6 +2,12 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
+import "src/policies/DLPVault.sol";
+import "src/policies/Leverager.sol";
+import "src/policies/StakeChef.sol";
+import "src/policies/RolesAdmin.sol";
+
+import "src/Kernel.sol";
 
 contract CounterScript is Script {
     function setUp() public {}
