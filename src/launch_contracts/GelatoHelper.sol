da4e44d0ba510fa3df7ca05344dff37fbfdf3b1c
w <radiateprotocol@proton.me>
2023-07-11 17:12:32 -0700

    finish up gelatohelper, refactor code for readabilit

--- src/launch_contracts/GelatoHelper.sol
+++ src/launch_contracts/GelatoHelper.sol
@@ -5,96 +5,102 @@ import {IAToken} from "src/interfaces/radiant-interfaces/IAToken.sol";
 import {Leverager} from "src/policies/Leverager.sol";
 import {DLPVault} from "src/policies/DLPVault.sol";
 import {ILendingPool} from "src/interfaces/radiant-interfaces/ILendingPool.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {DLPVault} from "src/policies/DLPVault.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "src/interfaces/uniswap/IUniswapV2Router02.sol";
 import "@solmate/mixins/ERC4626.sol";
 
-contract GelatoKeeper {
+contract GelatoKeeper is Ownable {
     using SafeERC20 for IERC20;
 
+    ///////////// STATE ///////////////
     IUniswapV2Router02 public immutable uniswapRouter;
     uint256 public constant RATIO_DIVISOR = 10000;
     ILendingPool public constant pool =
         ILendingPool(0xF4B1486DD74D07706052A33d31d7c0AAFD0659E1);
+    DLPVault public constant rdLP =
+        DLPVault(0xC6dC7749781F7Ba1e9424704B2904f2F94D3eb63);
+    ERC20 public constant WETH =
+        ERC20(0x82aF49447D8a07e3bd95BD0d56f35241523fBab1);
+
+    address public gelatoExecutor;
+    uint256 public dlpAPY;
     IAToken[] public aTokens;
     IERC20[] public rewardTokens;
     ERC4626[] public vaults;
     mapping(ERC4626 => uint) vaultAPY;
-    ERC4626 public constant rdLP =
-        ERC4626(0xC6dC7749781F7Ba1e9424704B2904f2F94D3eb63);
-    ERC20 public constant WETH =
-        ERC20(0x82aF49447D8a07e3bd95BD0d56f35241523fBab1);
-    uint256 public dlpAPY;
-    address public owner;
+    uint256 public lastExecuted;
 
-    constructor(IUniswapV2Router02 _uniswapRouter) {
+    constructor(IUniswapV2Router02 _uniswapRouter) Ownable() {
         uniswapRouter = _uniswapRouter;
-        owner = msg.sender;
+        // Ensure that the contract is approved as a Keeper role
     }
 
-    function addAtoken(IAToken[] memory _tokens) external {
-        require(msg.sender == owner, "Only owner can add tokens");
+    ///////////// ADMIN FUNCTIONS ///////////////
+
+    function addAtoken(IAToken[] memory _tokens) external onlyOwner {
         aTokens = _tokens;
     }
 
-    function addRewardToken(IERC20[] memory _tokens) external {
-        require(msg.sender == owner, "Only owner can add tokens");
+    function addRewardToken(IERC20[] memory _tokens) external onlyOwner {
         rewardTokens = _tokens;
+        for (uint i = 0; i < _tokens.length; i++) {
+            if (
+                _tokens[i].allowance(address(this), address(uniswapRouter)) == 0
+            ) {
+                _tokens[i].approve(address(uniswapRouter), type(uint256).max);
+            }
+        }
         // Should be ordered by vaults array and then any extra tokens should be added to the end
         // last vault token should be WETH
     }
 
-    function addRewardToken(IERC20 token) external {
-        require(msg.sender == owner, "Only owner can add tokens");
+    function addRewardToken(IERC20 token) external onlyOwner {
         rewardTokens.push(token);
+        if (token.allowance(address(this), address(uniswapRouter)) == 0) {
+            token.approve(address(uniswapRouter), type(uint256).max);
+        }
     }
 
-    function addVault(ERC4626[] memory _vaults) external {
-        require(msg.sender == owner, "Only owner can add vaults");
+    function addVault(ERC4626[] memory _vaults) external onlyOwner {
         vaults = _vaults;
         // should be ordered in order of yield priority
     }
 
-    function addVault(ERC4626 _vault) external {
-        require(msg.sender == owner, "Only owner can add vaults");
+    function addVault(ERC4626 _vault) external onlyOwner {
         vaults.push(_vault);
     }
 
-    function setAPY(uint256 _apy, ERC4626 _vault) external {
-        require(msg.sender == owner, "Only owner can set APY");
+    function setAPY(uint256 _apy, ERC4626 _vault) external onlyOwner {
         vaultAPY[_vault] = _apy;
     }
 
-    function setdLPAPY(uint256 _apy) external {
-        require(msg.sender == owner, "Only owner can set APY");
+    function setdLPAPY(uint256 _apy) external onlyOwner {
         dlpAPY = _apy;
+    }
+
+    function setGelatoExecutor(address _executor) external onlyOwner {
+        gelatoExecutor = _executor;
+    }
+
+    function recoverTokens(IERC20 token) external onlyOwner {
+        token.transfer(msg.sender, token.balanceOf(address(this)));
     }
 
     function getAPY(ERC4626 _vault) external view returns (uint256) {
         return vaultAPY[_vault];
     }
 
-    function transferOwner(address _owner) external {
-        require(msg.sender == owner, "Only owner can transfer ownership");
-        owner = _owner;
-    }
-
-    function processATokens() internal {
-        for (uint i = 0; i < aTokens.length; i++) {
-            address _underlying = aTokens[i].UNDERLYING_ASSET_ADDRESS();
-
-            // Withdraw the aToken to get the underlying asset
-            uint256 aTokenBalance = aTokens[i].balanceOf(address(this));
-
-            pool.withdraw(_underlying, aTokenBalance, address(this));
-        }
-    }
-
     function disperseRewards() external {
+        require(msg.sender == gelatoExecutor, "Not gelato executor");
+
+        rdLP.claim();
         // convert any non aToken rewards to aTokens
         processATokens();
-        uint256[] memory rewardsNeeded = calculateAPYs();
+        // Pay down tokens where underlying is greater than reward debt, and then receive rewards needed
+        uint256[] memory rewardsNeeded = payAndCalculate();
 
         // Process rdLP rewards
         uint256 rewardAmt = (rdLP.totalAssets() * dlpAPY) / RATIO_DIVISOR >=
@@ -103,21 +109,28 @@ contract GelatoKeeper {
             : (rdLP.totalAssets() * dlpAPY) / RATIO_DIVISOR;
 
         WETH.transfer(address(rdLP), rewardAmt);
+        updaterDLPPeriod();
+        rdLP.notifyRewardAmount(rewardAmt);
 
+        // Process each vault reward
         for (uint i = 0; i < vaults.length; i++) {
             if (rewardsNeeded[i] == 0) {
                 continue;
             } else {
                 // Need to swap any reward tokens to the vault asset to pay down rewards
-                for (uint j = 0; j < rewardTokens.length; j++) {
+                for (uint256 j = 0; j < rewardTokens.length; j++) {
                     uint256 _balance = rewardTokens[i].balanceOf(address(this));
                     if (rewardsNeeded[i] == 0) {
+                        // Rewards paid down
                         break;
+                    } else if (_balance == 0) {
+                        // No reward tokens to swap
+                        continue;
                     } else if (
                         _balance > 0 &&
                         address(rewardTokens[j]) == address(vaults[i].asset())
                     ) {
-                        // pay out balance with any rewards
+                        // pay out rewards with current balance of Reward token
                         uint256 rewardPayoff = rewardsNeeded[i] <= _balance
                             ? rewardsNeeded[i]
                             : _balance;
@@ -126,21 +139,8 @@ contract GelatoKeeper {
                             address(vaults[i]),
                             rewardPayoff
                         );
-                    } else if (_balance == 0) {
-                        continue;
                     } else {
-                        // Swap the asset for the respective asset of the vault
-                        if (
-                            rewardTokens[i].allowance(
-                                address(this),
-                                address(uniswapRouter)
-                            ) == 0
-                        ) {
-                            rewardTokens[i].approve(
-                                address(uniswapRouter),
-                                type(uint256).max
-                            );
-                        }
+                        // Approve is done when tokens are added
                         uint256[] memory amounts = uniswapRouter
                             .swapExactTokensForTokens(
                                 _balance,
@@ -165,9 +165,23 @@ contract GelatoKeeper {
                 }
             }
         }
+        lastExecuted = block.timestamp;
     }
 
-    function calculateAPYs() internal returns (uint256[] memory) {
+    ///////////// INTERNAL FUNCTIONS ///////////////
+
+    function processATokens() internal {
+        for (uint i = 0; i < aTokens.length; i++) {
+            address _underlying = aTokens[i].UNDERLYING_ASSET_ADDRESS();
+
+            // Withdraw the aToken to get the underlying asset
+            uint256 aTokenBalance = aTokens[i].balanceOf(address(this));
+
+            pool.withdraw(_underlying, aTokenBalance, address(this));
+        }
+    }
+
+    function payAndCalculate() internal returns (uint256[] memory) {
         uint256[] memory rewardAmt = new uint256[](vaults.length);
         for (uint i = 0; i < vaults.length; i++) {
             rewardAmt[i] =
@@ -198,8 +212,22 @@ contract GelatoKeeper {
         return path;
     }
 
-    function recoverTokens(IERC20 token) external {
-        require(msg.sender == owner, "Only owner can recover tokens");
-        token.transfer(owner, token.balanceOf(address(this)));
+    function updaterDLPPeriod() public {
+        if (rdLP.finishAt() < block.timestamp) {
+            // Need to reset it
+            rdLP.setRewardsDuration(7 days);
+        }
+    }
+
+    // Checker for Gelato Keeper
+    function checker() external view returns (bool, bytes memory) {
+        if (lastExecuted > block.timestamp - 7 days) {
+            return (false, "");
+        } else {
+            return (
+                true,
+                abi.encodeWithSelector(this.disperseRewards.selector)
+            );
+        }
     }
 }
